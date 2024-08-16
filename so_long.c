b979682fdf0c0732bc72d086e98812a1c2dd6a4e
Analdo Manuel <analdomanuel18@gmail.com>
2024-08-16 14:58:57 +0100

    so_long

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: almanuel <almanuel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 11:58:32 by almanuel          #+#    #+#             */
-/*   Updated: 2024/08/15 17:46:38 by almanuel         ###   ########.fr       */
+/*   Updated: 2024/08/16 14:40:27 by almanuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,14 +47,15 @@ void    open_game(t_data *zona, int ix, int iy)
         while (x < zona->point.x)
         {
             if (zona->ptr[y][x] == '1')
-                zona->img = mlx_xpm_file_to_image(zona->mlx, "imagens/parede.xpm", &zona->img_x, &zona->img_y);
+                mlx_put_image_to_window(zona->mlx, zona->mlx_win, zona->img_wall, ix, iy);
             else if (zona->ptr[y][x] == 'P')
-                zona->img = mlx_xpm_file_to_image(zona->mlx, "imagens/parado.xpm", &zona->img_x, &zona->img_y);
+                mlx_put_image_to_window(zona->mlx, zona->mlx_win, zona->img_player, ix, iy);
             else if (zona->ptr[y][x] == 'C')
-                zona->img = mlx_xpm_file_to_image(zona->mlx, "imagens/fruta.xpm", &zona->img_x, &zona->img_y);
+                mlx_put_image_to_window(zona->mlx, zona->mlx_win, zona->img_col, ix, iy);
+            else if (zona->ptr[y][x] == 'E')
+                mlx_put_image_to_window(zona->mlx, zona->mlx_win, zona->img_exit, ix, iy);
             else
-                zona->img = mlx_xpm_file_to_image(zona->mlx, "imagens/fundo.xpm", &zona->img_x, &zona->img_y);
-            mlx_put_image_to_window(zona->mlx, zona->mlx_win, zona->img, ix, iy);
+                mlx_put_image_to_window(zona->mlx, zona->mlx_win, zona->img_sp, ix, iy);
             x++;
             ix +=50;
         }
@@ -62,12 +63,45 @@ void    open_game(t_data *zona, int ix, int iy)
         iy +=50;
     }
 }
+size_t    init_img(t_data *zona)
+{
+    zona->img_player = mlx_xpm_file_to_image(zona->mlx, "imagens/parado.xpm", &zona->img_x, &zona->img_y);
+    if (!zona->img_player)
+        return (0);
+    zona->img_col = mlx_xpm_file_to_image(zona->mlx, "imagens/fruta.xpm", &zona->img_x, &zona->img_y);
+    if (!zona->img_col)
+        return (0);
+    zona->img_wall = mlx_xpm_file_to_image(zona->mlx, "imagens/parede.xpm", &zona->img_x, &zona->img_y);
+    if (!zona->img_wall)
+        return (0);
+    zona->img_sp = mlx_xpm_file_to_image(zona->mlx, "imagens/fundo.xpm", &zona->img_x, &zona->img_y);
+    if (!zona->img_sp)
+        return (0);
+    zona->img_exit = mlx_xpm_file_to_image(zona->mlx, "imagens/porta.xpm", &zona->img_x, &zona->img_y);
+    if (!zona->img_exit)
+        return (0);
+    return (1);
+}
 
-int main(void)
+int    key_event(int keycode, void *param)
+{
+    static size_t  count = 1;
+    printf("%ld\n", count++);
+    if (keycode == 65307) { // Tecla 'Esc' em muitos sistemas
+        printf("Tecla 'Esc' pressionada.\n");
+        mlx_loop_end(param); // Encerra o loop principal da janela
+    } else {
+        printf("Tecla com código %d pressionada.\n", keycode);
+    }
+    return (0);
+}
+int main(int ac, char **av)
 {
     int ix;
     int iy;
 
+    (void)ac;
+    (void)av;
     ix = 0;
     iy = 0;
     t_data  zona;
@@ -79,19 +113,14 @@ int main(void)
     zona.mlx_win = mlx_new_window(zona.mlx, zona.point.x*50, zona.point.y*50, "so_long");
     if (zona.mlx_win == NULL)
         mlx_destroy_window(zona.mlx, zona.mlx_win);
-    open_game(&zona, ix, iy);    
-    if (zona.img == NULL)
-    {
-        mlx_destroy_window(zona.map, zona.mlx_win);
-        mlx_destroy_display(zona.mlx);
-        free(zona.mlx);
-        return 1;
-    }
-    
+    if (!init_img(&zona))
+        printf("Fail Opening\n");
+    open_game(&zona, ix, iy);
+    mlx_key_hook(zona.mlx_win, key_event, zona.mlx);
     mlx_loop(zona.mlx);
+    
     ft_free(zona.map);
     ft_free(zona.ptr);
-    mlx_destroy_image(zona.mlx, zona.img);
     mlx_destroy_window(zona.mlx, zona.mlx_win);
     mlx_destroy_display(zona.mlx);
     free(zona.mlx);
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: almanuel <almanuel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/12 12:00:18 by almanuel          #+#    #+#             */
-/*   Updated: 2024/08/15 16:57:00 by almanuel         ###   ########.fr       */
+/*   Updated: 2024/08/16 14:29:48 by almanuel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,16 +29,19 @@ typedef struct s_data
 {
     void    *mlx;
     void    *mlx_win;
-    void    *img;
+    void    *img_player;
+    void    *img_exit;
+    void    *img_col;
+    void    *img_wall;
+    void    *img_sp;
     char    **ptr;
     char    **map;
-    t_point     point;
     size_t  C;
-    size_t  s_C;
     size_t  P;
     size_t  E;
     int     img_x;
     int     img_y;
+    t_point     point;
 }              t_data;
 
 void    open_zona(t_data *zona);
